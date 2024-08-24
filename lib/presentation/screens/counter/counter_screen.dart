5593a38f2e4261e487e6f308ceb40675e34bb5ee
Cristian Castro <kencrisgdev@gmail.com>
2024-08-23 21:24:03 -0500

    Firt Commit

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickcounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickcounter',
+              style:
+                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            const Text('Clicks',
+                style: TextStyle(
+                  fontSize: 25,
+                )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickcounter++;
+          });
+        },
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
